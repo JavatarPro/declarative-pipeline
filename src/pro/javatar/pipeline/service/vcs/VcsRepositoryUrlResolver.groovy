068046fcee6b78f845aeaa99e1b3f59165fa8021
Borys Zora <bzora@yaypay.com>
2018-05-26 12:05:21 +0300

    amend getRepoUrl (change pattern) for bitbucket ssh

--- src/pro/javatar/pipeline/service/vcs/VcsRepositoryUrlResolver.groovy
+++ src/pro/javatar/pipeline/service/vcs/VcsRepositoryUrlResolver.groovy
@@ -55,7 +55,7 @@ class VcsRepositoryUrlResolver {
         }
         if (vcsRepositoryType == VcsRepositoryType.BITBUCKET) {
             if (useSsh) {
-                return "git@${domain}:${repoOwner}/${repoName}.git"
+                return "ssh://git@${domain}/${repoOwner}/${repoName}.git"
             }
             return "https://${userName}@bitbucket.org/${repoOwner}/${repoName}.git/"
         }
