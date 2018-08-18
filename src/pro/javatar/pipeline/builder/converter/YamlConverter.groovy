841240a4870417cc98a9b81c33607840f3aaefc5
Borys Zora <borys.zora@javatar.pro>
2018-08-18 14:09:37 +0300

    try to read Yaml 22

--- src/pro/javatar/pipeline/builder/converter/YamlConverter.groovy
+++ src/pro/javatar/pipeline/builder/converter/YamlConverter.groovy
@@ -1,5 +1,6 @@
 package pro.javatar.pipeline.builder.converter
 
+import pro.javatar.pipeline.builder.Maven
 import pro.javatar.pipeline.builder.Npm
 import pro.javatar.pipeline.builder.YamlModel
 
@@ -8,7 +9,19 @@ class YamlConverter {
     YamlModel toYamlModel(def yml) {
         YamlModel model = new YamlModel()
         populateNpm(model, yml)
+        populateMaven(model, yml)
         return model
+    }
+
+    def populateMaven(YamlModel model, def yml) {
+        def maven = yml.maven
+        model.setNpm(new Maven()
+                .withRepositoryId(maven.repository.id)
+                .withRepoUrl(maven.repository.url)
+                .withMaven(maven.jenkins-tools.maven)
+                .withJava(maven.jenkins-tools.java)
+                .withMavenParams(maven.params)
+        )
     }
 
     def populateNpm(YamlModel model, def yml) {
