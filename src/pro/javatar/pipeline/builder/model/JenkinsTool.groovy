6709c72255b7db861882c5e07fffb8027136afd5
Borys Zora <borys.zora@javatar.pro>
2018-08-19 00:51:20 +0300

    try to read Yaml 34

--- src/pro/javatar/pipeline/builder/model/JenkinsTool.groovy
+++ src/pro/javatar/pipeline/builder/model/JenkinsTool.groovy
@@ -6,6 +6,69 @@ class JenkinsTool {
 
     String maven
 
-    String npm
+    String npmVersion
 
+    String npmType
+
+    String getJava() {
+        return java
+    }
+
+    void setJava(String java) {
+        this.java = java
+    }
+
+    JenkinsTool withJava(String java) {
+        this.java = java
+        return this
+    }
+
+    String getMaven() {
+        return maven
+    }
+
+    void setMaven(String maven) {
+        this.maven = maven
+    }
+
+    JenkinsTool withMaven(String maven) {
+        this.maven = maven
+        return this
+    }
+
+    String getNpmVersion() {
+        return npmVersion
+    }
+
+    void setNpmVersion(String npmVersion) {
+        this.npmVersion = npmVersion
+    }
+
+    JenkinsTool withNpmVersion(String npmVersion) {
+        this.npmVersion = npmVersion
+        return this
+    }
+
+    String getNpmType() {
+        return npmType
+    }
+
+    void setNpmType(String npmType) {
+        this.npmType = npmType
+    }
+
+    JenkinsTool withNpmType(String npmType) {
+        this.npmType = npmType
+        return this
+    }
+
+    @Override
+    public String toString() {
+        return "JenkinsTool{" +
+                "java='" + java + '\'' +
+                ", maven='" + maven + '\'' +
+                ", npmVersion='" + npmVersion + '\'' +
+                ", npmType='" + npmType + '\'' +
+                '}';
+    }
 }
