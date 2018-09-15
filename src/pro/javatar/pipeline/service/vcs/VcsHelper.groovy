3c3986aa722a8f7e62b513fcfe6e8bde5cae573f
Borys Zora <borys.zora@javatar.pro>
2018-09-16 02:38:50 +0300

    try to fix, mesos configuration 11

--- src/pro/javatar/pipeline/service/vcs/VcsHelper.groovy
+++ src/pro/javatar/pipeline/service/vcs/VcsHelper.groovy
@@ -33,7 +33,7 @@ class VcsHelper {
         // TODO vcsRepo.revisionControlType ignored, used only common RevisionControlService
         RevisionControlService revisionControlService = ServiceContextHolder.getService(RevisionControlService.class)
         dsl.echo "VcsHelper: checkoutRepo: revisionControlService: ${revisionControlService} will be used"
-        VscCheckoutRequest request = VcsConverter.toVscCheckoutRequest(vcsRepo)
+        VscCheckoutRequest request = VcsConverter.toVscCheckoutRequest(vcsRepo, revisionControlService)
         dsl.dir(folder) {
             dsl.echo "before call revisionControlService.checkoutRepo(request: ${request})"
             revisionControlService.checkoutRepo(request)
--- src/pro/javatar/pipeline/service/vcs/converter/VcsConverter.groovy
+++ src/pro/javatar/pipeline/service/vcs/converter/VcsConverter.groovy
@@ -1,6 +1,7 @@
 package pro.javatar.pipeline.service.vcs.converter
 
 import pro.javatar.pipeline.service.ServiceContextHolder
+import pro.javatar.pipeline.service.vcs.RevisionControlService
 import pro.javatar.pipeline.service.vcs.VcsRepositoryUrlResolver
 import pro.javatar.pipeline.service.vcs.model.VcsRepo
 import pro.javatar.pipeline.service.vcs.model.VscCheckoutRequest
@@ -9,10 +10,12 @@ import static pro.javatar.pipeline.service.PipelineDslHolder.dsl
 
 class VcsConverter {
 
-    static VcsRepositoryUrlResolver urlResolver = ServiceContextHolder.getService(VcsRepositoryUrlResolver.class)
+    // static VcsRepositoryUrlResolver urlResolver = ServiceContextHolder.getService(VcsRepositoryUrlResolver.class)
 
-    static VscCheckoutRequest toVscCheckoutRequest(VcsRepo vcsRepo) {
+    static VscCheckoutRequest toVscCheckoutRequest(VcsRepo vcsRepo, RevisionControlService revisionControlService) {
         dsl.echo "VcsConverter: toVscCheckoutRequest: vcsRepo: ${vcsRepo}"
+        VcsRepositoryUrlResolver urlResolver = new VcsRepositoryUrlResolver(vcsRepo.getType(), vcsRepo.getSsh(),
+                revisionControlService)
         String repoUrl = urlResolver.getRepoUrl(vcsRepo)
         return new VscCheckoutRequest()
                 .withCredentialsId(vcsRepo.getCredentialsId())
