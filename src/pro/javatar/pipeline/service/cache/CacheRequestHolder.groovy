9d0069060d8e38d6973abac3230e5216afc8800f
Borys Zora <bzora@yaypay.com>
2018-10-11 17:07:32 +0300

    try to fix CacheRequestHolder

--- src/pro/javatar/pipeline/service/cache/CacheRequestHolder.groovy
+++ src/pro/javatar/pipeline/service/cache/CacheRequestHolder.groovy
@@ -3,18 +3,18 @@ package pro.javatar.pipeline.service.cache
 class CacheRequestHolder {
 
     // Map<service, List<folder path>>
-    static Map<String, List<String>> caches = new HashMap<>()
+    private static Map<String, List<String>> caches = new HashMap<>()
 
     static List<String> getCaches(String service) {
         return caches.get(service)
     }
 
     static void setCaches(Map<String, List<String>> cacheMap) {
-        caches = cacheMap
+        caches.putAll(cacheMap)
     }
 
     static CacheRequestHolder addCache(String service, String folder) {
-        this.caches.put(folder)
+        caches.put(folder)
         return this
     }
 
