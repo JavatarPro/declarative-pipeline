3e8b7e476817d5ccdf800ca4d68fc12ab32259b0
Borys Zora <bzora@yaypay.com>
2018-10-11 17:13:19 +0300

    fix creation of folder hierarchy

--- src/pro/javatar/pipeline/service/cache/CacheService.groovy
+++ src/pro/javatar/pipeline/service/cache/CacheService.groovy
@@ -18,7 +18,7 @@ class CacheService {
         dsl.echo "CacheService: createFolderWorkspaceSymbolicLink with folderToBeCached: ${folderToBeCached} " +
                 "& alias ${alias} started"
         String buildFolder = dsl.env.WORKSPACE
-        String cacheFolder = "${buildFolder}/${alias}"
+        String cacheFolder = "${buildFolder}/cache/${alias}"
         String repoFolder = "${buildFolder}/repo/${folderToBeCached}"
         createFolderSymbolicLink(cacheFolder, repoFolder)
         dsl.echo "CacheService: createFolderWorkspaceSymbolicLink finished"
@@ -28,7 +28,7 @@ class CacheService {
         dsl.echo "CacheService: createFolderSymbolicLink with cacheFolder: ${targetFolder} " +
                 "& targetFolder ${targetFolder} started"
         if (! dsl.fileExists(cacheFolder)) {
-            dsl.sh "mkdir ${cacheFolder}"
+            dsl.sh "mkdir -p ${cacheFolder}"
         }
         dsl.echo "ln -s ${cacheFolder} ${targetFolder}"
         dsl.sh "ln -s ${cacheFolder} ${targetFolder}"
