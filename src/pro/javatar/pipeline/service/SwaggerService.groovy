415e4af5607c658ab097adbcbc50b51137b853f6
Borys Zora <borys.zora@javatar.pro>
2019-03-29 23:35:10 +0200

    CD-16: update licence url

--- src/pro/javatar/pipeline/service/SwaggerService.groovy
+++ src/pro/javatar/pipeline/service/SwaggerService.groovy
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://github.com/JavatarPro/pipeline-utils/blob/master/LICENSE
+ *     https://github.com/JavatarPro/declarative-pipeline/blob/master/LICENSE
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
