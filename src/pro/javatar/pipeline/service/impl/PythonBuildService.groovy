fcb9dd6b7e4948d9a2cddcf127e9aa79107d87cc
Serhii Petrychenko <serhii.petrychenko@javatar.pro>
2019-11-04 23:52:25 +0200

    install dependencies

--- src/pro/javatar/pipeline/service/impl/PythonBuildService.groovy
+++ src/pro/javatar/pipeline/service/impl/PythonBuildService.groovy
@@ -29,6 +29,7 @@ class PythonBuildService extends BuildService {
     @Override
     void buildAndUnitTests(ReleaseInfo releaseInfo) {
         Logger.info("PythonBuildService buildAndUnitTests started")
+        dsl.sh "pip install -r requirements.txt --no-index --find-links file:///tmp/packages"
         dsl.sh "python -m unittest discover -s tests"
         dockerService.dockerBuildImage(releaseInfo.getDockerImageName(), releaseInfo.getDockerImageVersion())
         Logger.info("PythonBuildService buildAndUnitTests finished")
